7841b7e031361ded3c3ac32d84030e3c9af1d3d7
Panda <connordeleo@gmail.com>
2023-09-17 19:15:04 -0700

    ???

--- AP Calculus/modifiedIVT/main.py
+++ AP Calculus/modifiedIVT/main.py
@@ -1,27 +1,40 @@
+from symfit import parameters, variables, Fit, Piecewise, exp, Eq, Model
+import numpy as np
+import matplotlib.pyplot as plt
 import random as rand
-import string
-import os
-import tkinter as tk
 
-def func(domain, func):
-    x = (domain, domain + 50)
+x, y = variables('x, y')
+a, b, x0 = parameters('a, b, x0')
 
-    if func == 1:
-        y = rand(0,100)
-    elif func == 2:
-        y = (rand(-1,1) * x) + rand(0,16)
-    elif func == 3:
-        degree = rand(2,5)
-        
-        if degree == 2:
-            y = (rand(1,16) * pow(x, 2)) + (rand(0,16) * x) + rand(0,16)
-        elif degree == 3:
-            y = (rand(1,16) * pow(x, 3)) + (rand(0,16) * pow(x, 2)) + (rand(0,16) * x) + rand(0,16)
-        elif degree == 4:
-            y = (rand(1,16) * pow(x, 4)) + (rand(0,16) * pow(x, 3)) + (rand(0,16) * pow(x, 2)) + (rand(0,16) * x) + rand(0,16)
-        elif degree == 5:
-            y = (rand(1,16) * pow(x, 5)) + (rand(0,16) * pow(x, 4)) + (rand(0,16) * pow(x, 3)) + (rand(0,16) * pow(x, 2)) + (rand(0,16) * x) + rand(0,16)
+# Make a piecewise model
+func = rand.randint(1,2)
+if func == 1:
+    y1 = x**2 - a * x
+    y2 = a * x + b
+else:
+    y1 = a * x + b
+    y2 = x**2 - a * x
 
-def checker():
-    print("Checker")
+model = Model({y: Piecewise((y1, x <= x0), (y2, x > x0))})
 
+# As a constraint, we demand equality between the two models at the point x0
+# to do this, we substitute x -> x0 and demand equality using `Eq`
+constraints = [
+    Eq(y1.subs({x: x0}), y2.subs({x: x0}))
+]
+# Generate example data
+xdata = np.linspace(-4, 4., 50)
+ydata = model(x=xdata, a=0.0, b=1.0, x0=1.0).y
+np.random.seed(4)
+ydata = np.random.normal(ydata, 0.5)  # add noise
+
+# Help the fit by bounding the switchpoint between the models
+x0.min = 0.8
+x0.max = 1.2
+
+fit = Fit(model, x=xdata, y=ydata, constraints=constraints)
+fit_result = fit.execute()
+
+plt.scatter(xdata, ydata)
+plt.plot(xdata, model(x=xdata, **fit_result.params).y)
+plt.show()
